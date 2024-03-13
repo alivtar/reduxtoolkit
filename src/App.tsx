fcfb8ba8888b3b186b24715b3217eb40c86dcbc6
Ali Baghban <ali.angori007@gmail.com>
2024-03-14 01:18:52 +0330

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/contact"}>Contact</Link>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login/index.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div>
+      <h1>This is the Login page</h1>
+      <input />
+      <button>Submit Login</button>
+    </div>
+  );
+}
+
+export default Login;
